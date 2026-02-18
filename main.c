8ac3b9e681a3bcb73ef71a5a2664f0b38867ce4a
Иван Курносов <ivonkurn@gmail.com>
2026-02-18 13:37:44 +0700

    Написана логика определения слов анаграммой

--- main.c
+++ main.c
@@ -2,6 +2,47 @@
 
 int main()
 {
-    printf("Hello World!\n");
+    char w1[7] = "komjr";
+    char w2[7] = "korma";
+
+    int Len_w1 = 0;
+    int Len_w2 = 0;
+    int i = 0;
+    while (w1[i]){
+        Len_w1++;
+        i++;
+    }
+
+    i = 0;
+    while (w2[i]){
+        Len_w2++;
+        i++;
+    }
+
+    if (Len_w1 != Len_w2) {
+        printf("The words W1 and W2 have different lengths. They can't be anagrams.\n");
+        return 0;
+    }
+
+    int Tab_w1[256] = {0};
+    for (int j = 0; j < Len_w1; j++){
+        int letter = w1[j];
+        Tab_w1[letter]++;
+    }
+
+    int Tab_w2[256] = {0};
+    for (int j = 0; j < Len_w2; j++){
+        int letter = w2[j];
+        Tab_w2[letter]++;
+    }
+
+    int flag = 0;
+    for (int j = 0; (j < 256) && (flag == 0); j++){
+        if (Tab_w1[j] != Tab_w2[j]){
+            flag = 1;
+        }
+    }
+    if (flag == 0) printf("Yes.\n");
+    else printf("No.\n");
     return 0;
 }
