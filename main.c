b4de9c8562d9082e9ecbb1e3539947f6c51d6bbb
ivonk <ivonkurn@gmail.com>
2026-02-23 13:58:07 +0300

    Написана логика для уплотнения строки

--- main.c
+++ main.c
@@ -2,6 +2,26 @@
 
 int main()
 {
-    printf("Hello World!\n");
+    char s1[15] = "Helloh People!";
+    char s2[7] = "ople";
+
+    int Tab[256] = {0};
+    char *ptr_s2 = NULL;
+    ptr_s2 = s2;
+
+    while (*ptr_s2) {
+        Tab[(unsigned char)*ptr_s2++] = 1;
+    }
+
+    char new_s1[11] = "";
+    char *ptr_write = new_s1;
+    for (char *ptr_read = s1; *ptr_read != '\0'; ptr_read++){
+      if (Tab[(unsigned char) *ptr_read] == 0){
+        *ptr_write = *ptr_read;
+        ptr_write++;
+      }
+    }
+    *ptr_write = '\0';
+    printf("%s", new_s1);
     return 0;
 }
