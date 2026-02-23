4313355c5dd6e0b438150fd2981a9b27d2181aac
ivonk <ivonkurn@gmail.com>
2026-02-23 14:29:47 +0300

    Написана основная логика для нахождения слова с двумя "d"

--- main.c
+++ main.c
@@ -2,6 +2,30 @@
 
 int main()
 {
-    printf("Hello World!\n");
-    return 0;
+    char str[35] = "add,door,dad,data,added.";
+
+    char word[100];
+    int wordLen = 0;
+    int dCount = 0;
+
+    for (int i = 0; str[i] != '\0'; i++) {
+      char letter = str[i];
+
+      if (letter != ',' && letter != '.') {
+        word[wordLen++] = letter;
+        if (letter == 'd') {
+          dCount++;
+        }
+      }
+      else {
+        word[wordLen] = '\0';
+
+        if (dCount == 2) {
+          printf("%s\n", word);
+        }
+
+        wordLen = 0;
+        dCount = 0;
+      }
+    }
 }
