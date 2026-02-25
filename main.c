97f0e5d0f54d1bd773f875b38c01a1fdb6b1cf52
Иван Курносов <ivonkurn@gmail.com>
2026-02-25 18:00:23 +0700

    Переписано на функции

--- main.c
+++ main.c
@@ -1,4 +1,43 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+void Fill_associative_table(int *Tab, char *str, size_t size){
+    for (size_t i = 0; i < size; i++){
+        unsigned char letter = str[i];
+        Tab[letter]++;
+    }
+}
+
+bool Check_anagram(char *w1, char *w2){
+    size_t Len_w1 = strlen(w1);
+    size_t Len_w2 = strlen(w2);
+
+    if (Len_w1 != Len_w2) {
+        printf("The words W1 and W2 have different lengths. They can't be anagrams.\n");
+        return false;
+    }
+
+    if (Len_w1 == 0 || Len_w2 == 0){
+        printf("No words.\n");
+        return false;
+    }
+
+    int Tab_w1[256] = {0};
+    Fill_associative_table(Tab_w1, w1, Len_w1);
+
+    int Tab_w2[256] = {0};
+    Fill_associative_table(Tab_w2, w2, Len_w2);
+
+    bool flag = true;
+    for (int j = 0; (j < 256) && (flag == true); j++){
+        if (Tab_w1[j] != Tab_w2[j]){
+            flag = false;
+        }
+    }
+    return flag;
+}
+
 
 int main()
 {
@@ -17,49 +56,7 @@ int main()
     char w1[7] = "komar"; // Yes.
     char w2[7] = "komar";
 
-    int Len_w1 = 0;
-    int Len_w2 = 0;
-    int i = 0;
-    while (w1[i]){
-        Len_w1++;
-        i++;
-    }
-
-    i = 0;
-    while (w2[i]){
-        Len_w2++;
-        i++;
-    }
-
-    if (Len_w1 != Len_w2) {
-        printf("The words W1 and W2 have different lengths. They can't be anagrams.\n");
-        return 0;
-    }
-
-    if (Len_w1 == 0 || Len_w2 == 0){
-        printf("No words.\n");
-        return 0;
-    }
-
-    int Tab_w1[256] = {0};
-    for (int j = 0; j < Len_w1; j++){
-        int letter = w1[j];
-        Tab_w1[letter]++;
-    }
-
-    int Tab_w2[256] = {0};
-    for (int j = 0; j < Len_w2; j++){
-        int letter = w2[j];
-        Tab_w2[letter]++;
-    }
-
-    int flag = 0;
-    for (int j = 0; (j < 256) && (flag == 0); j++){
-        if (Tab_w1[j] != Tab_w2[j]){
-            flag = 1;
-        }
-    }
-    if (flag == 0) printf("Yes.\n");
+    if (Check_anagram(w1, w2) == true) printf("Yes.\n");
     else printf("No.\n");
     return 0;
 }
