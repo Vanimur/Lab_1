7b8875cb696c178b7ed299c1f6b1d668dbe843b4
ivonk <ivonkurn@gmail.com>
2026-03-01 19:14:14 +0300

    Переделана логика вывода (передача кол-ва значений)

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdbool.h>
 #include <string.h>
 
-bool Find_words_with_two_d(char *str, int *out_index){
+int Find_words_with_two_d(char *str, int *out_index){
     int index_Count = 0;
     int d_Count = 0;
     int word_start = 0;
@@ -16,7 +16,7 @@ bool Find_words_with_two_d(char *str, int *out_index){
       bool is_end = (letter == '\0');
 
       if (!is_letter && !is_limiter && !is_end) {
-          return 0;
+        return 0;
       }
 
       if (is_letter) {
@@ -40,11 +40,11 @@ bool Find_words_with_two_d(char *str, int *out_index){
         in_word = 0;
       }
     }
-    return 1;
+    return index_Count;
 }
 
-void Print_words_by_indices(char *str, int *out_index){
-    for (int i = 0; i < 100 && out_index[i] != -1; i++) {
+void Print_words_by_indices(char *str, int *out_index, int count){
+    for (int i = 0; i < count; i++) {
       int start = out_index[i];
       for (int j = start; str[j] != '\0'; j++) {
         char letter = str[j];
@@ -64,11 +64,13 @@ int main()
 
     int out_index[100];
     for (int i = 0; i < 100; i++) {
-        out_index[i] = -1;
+      out_index[i] = -1;
     }
 
-    if (Find_words_with_two_d(str, out_index)) {
-      Print_words_by_indices(str, out_index);
+    int found_count = Find_words_with_two_d(str, out_index);
+
+    if (found_count) {
+      Print_words_by_indices(str, out_index, found_count);
     }
     else {
       printf("Found invalid characters.\n");
