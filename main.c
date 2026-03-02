ce9e33ae282a19ccc4f5cc573ef48e51522c6326
ivonk <ivonkurn@gmail.com>
2026-03-02 14:10:33 +0300

    Анализ теста с русскими символами.

--- main.c
+++ main.c
@@ -45,6 +45,9 @@ int main()
 //    char w1[7] = "kaban"; // Yes.
 //    char w2[7] = "banka";
 
+    char w1[7] = "komar"; // Yes.
+    char w2[7] = "komar";
+
 //    char w1[7] = "kommar"; // The words W1 and W2 have different lengths. They can't be anagrams.
 //    char w2[7] = "korma";
 
@@ -55,8 +58,8 @@ int main()
 //    char w2[7] = "kolar";
 
 
-    char w1[7] = "komar"; // Yes.
-    char w2[7] = "komar";
+//    char w1[9] = "фафф"; // Yes.
+//    char w2[9] = "аффф";
 
     if (Check_anagram(w1, w2)) printf("Yes.\n");
     else printf("No.\n");
